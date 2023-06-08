cfee020e215c2d71bf97acf615d081867a4db916
lornz- <lrt.hrl@gmail.com>
2023-06-08 14:18:53 -0400

    Add helper functions for testing
    Allow to spin up a real mongodb server and start a process that stores data in memory and clean it out after tests

---
+++ src/lib/helpers/testHelpers.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-anonymous-default-export */
+// vendors
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import dbConnect from '../dbConnect';
+
+let mongoServer;
+
+// Provide connection to a new in-memory database server.
+const connect = async () => {
+  // before establishing a new connection close previous
+  // await mongoose.disconnect();
+
+  mongoServer = await MongoMemoryServer.create();
+
+  process.env.MONGODB_URI = await mongoServer.getUri();
+  await dbConnect();
+};
+
+// Remove and close the database and server.
+const close = async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+};
+
+// Remove all data from collections
+const clear = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    await collections[key].deleteMany();
+  }
+};
+
+export default {
+  connect,
+  close,
+  clear,
+};
