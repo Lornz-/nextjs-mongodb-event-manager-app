1a899e8cd61d08079415c0a3c40a5f4cfaae3b7b
lornz- <lrt.hrl@gmail.com>
2023-06-08 13:24:37 -0400

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/global';
+
+const theme = {
+  colors: {
+    primary: '#111',
+    secondary: '#0070f3',
+  },
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,145 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import styled from 'styled-components';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6rem;
+  min-height: 100vh;
+`;
+
+const Description = styled.div`
+  display: inherit;
+  justify-content: inherit;
+  align-items: inherit;
+  font-size: 0.85rem;
+  max-width: var(--max-width);
+  width: 100%;
+  z-index: 2;
+  font-family: var(--font-mono);
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  p {
+    position: relative;
+    margin: 0;
+    padding: 1rem;
+    background-color: rgba(var(--callout-rgb), 0.5);
+    border: 1px solid rgba(var(--callout-border-rgb), 0.3);
+    border-radius: var(--border-radius);
+  }
+`;
+
+const Code = styled.code`
+  font-weight: 700;
+  font-family: var(--font-mono);
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, minmax(25%, auto));
+  width: var(--max-width);
+  max-width: 100%;
+`;
+
+const Card = styled.div`
+  padding: 1rem 1.2rem;
+  border-radius: var(--border-radius);
+  background: rgba(var(--card-rgb), 0);
+  border: 1px solid rgba(var(--card-border-rgb), 0);
+  transition: background 200ms, border 200ms;
+
+  span {
+    display: inline-block;
+    transition: transform 200ms;
+  }
+
+  h2 {
+    font-weight: 600;
+    margin-bottom: 0.7rem;
+  }
+
+  p {
+    margin: 0;
+    opacity: 0.6;
+    font-size: 0.9rem;
+    line-height: 1.5;
+    max-width: 30ch;
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    :hover {
+      background: rgba(var(--card-rgb), 0.1);
+      border: 1px solid rgba(var(--card-border-rgb), 0.15);
+    }
+
+    :hover span {
+      transform: translateX(4px);
+    }
+  }
+
+  @media (prefers-reduced-motion) {
+    :hover span {
+      transform: none;
+    }
+  }
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  padding: 4rem 0;
+
+  ::before {
+    background: var(--secondary-glow);
+    border-radius: 50%;
+    width: 480px;
+    height: 360px;
+    margin-left: -400px;
+  }
+  ::after {
+    background: var(--primary-glow);
+    width: 240px;
+    height: 180px;
+    z-index: -1;
+  }
+  ::before,
+  ::after {
+    content: '';
+    left: 50%;
+    position: absolute;
+    filter: blur(45px);
+    transform: translateZ(0);
+  }
+`;
+
+const VercelLogo = styled.div`
+  @media (prefers-color-scheme: dark) {
+    filter: invert(1);
+  }
+`;
+
+const Logo = styled.div`
+  position: relative;
+
+  @media (prefers-color-scheme: dark) {
+    filter: invert(1) drop-shadow(0 0 0.3rem #ffffff70);
+  }
+`;
 
 export default function Home() {
   return (
@@ -14,11 +150,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
+
+      <Main className={inter.className}>
+        <Description>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
+            <Code>src/pages/index.js</Code>
           </p>
           <div>
             <a
@@ -37,9 +174,9 @@ export default function Home() {
               />
             </a>
           </div>
-        </div>
+        </Description>
 
-        <div className={styles.center}>
+        <Center>
           <Image
             className={styles.logo}
             src="/next.svg"
@@ -48,9 +185,9 @@ export default function Home() {
             height={37}
             priority
           />
-        </div>
+        </Center>
 
-        <div className={styles.grid}>
+        <Grid>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
@@ -107,8 +244,8 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
-        </div>
-      </main>
+        </Grid>
+      </Main>
     </>
-  )
+  );
 }
