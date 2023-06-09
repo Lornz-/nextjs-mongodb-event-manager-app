bc9760acee44e6aa4e481163a9d92e322bc47fe1
lornz- <lrt.hrl@gmail.com>
2023-06-08 23:38:07 -0400

    Rename file testHelpers.js -> testHelper.js

--- src/lib/helpers/testHelper.js
+++ src/lib/helpers/testHelper.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-// vendors
+// vendors
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import dbConnect from '../dbConnect';
