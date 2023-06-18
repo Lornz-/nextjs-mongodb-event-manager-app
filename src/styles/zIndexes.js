681ea6eebb846a39c594526860513cc4414d3ccc
lornz- <lrt.hrl@gmail.com>
2023-06-18 18:56:34 -0400

    Add z-indexes styles

---
+++ src/styles/zIndexes.js
@@ -0,0 +1,13 @@
+// vendors
+import { css } from 'styled-components';
+
+export default css`
+  --z-index-background: -1000;
+  --z-index-dropdown: 1000;
+  --z-index-sticky: 1020;
+  --z-index-fixed: 1030;
+  --z-index-modalBackdrop: 1040;
+  --z-index-modal: 1050;
+  --z-index-popover: 1060;
+  --z-index-tooltip: 1070;
+`;
