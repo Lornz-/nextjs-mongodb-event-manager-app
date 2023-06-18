e9db7e60a0fc3fc9598345177dd78534c9d9bfe1
lornz- <lrt.hrl@gmail.com>
2023-06-18 18:54:02 -0400

    Add colors styles

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+// vendors
+import { css } from 'styled-components';
+
+export default css`
+  --color-white: #fff;
+  --color-black: #000;
+  --color-light-grey: #efefef;
+  --color-neutral: #0e0f0c1f;
+  --color-primary: #1f0033;
+  --color-accent: #dba2ff;
+  --color-error: #c1121f;
+
+  --color-border-neutral: var(--color-neutral);
+  --color-border-primary: #0e0f0c59;
+`;
