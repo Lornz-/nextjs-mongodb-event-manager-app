5ac1d8f54fc6408248d4dd46adb7fe14a5fcbb26
lornz- <lrt.hrl@gmail.com>
2023-06-18 18:57:31 -0400

    Update global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,89 +1,74 @@
+// vendors
 import { createGlobalStyle } from 'styled-components';
 import { css } from 'styled-components';
 
+// styles
+import colors from './colors';
+import typography from './typography';
+import zIndexes from './zIndexes';
+import animations from './animations';
+
 export const rootStyle = css`
-  --max-width: 1100px;
-  --border-radius: 12px;
-  --font-mono: ui-monospace, Menlo, Monaco, 'Cascadia Mono', 'Segoe UI Mono',
-    'Roboto Mono', 'Oxygen Mono', 'Ubuntu Monospace', 'Source Code Pro',
-    'Fira Mono', 'Droid Sans Mono', 'Courier New', monospace;
+  ${colors};
+
+  ${typography};
+
+  ${zIndexes};
+
+  ${animations};
+
+  --container-max-width: 1440px;
+  --container-gutters: 16px;
+  --header-height: 200px;
+  --radius-small: 10px;
+  --radius-medium: 16px;
+  --radius-large: 24px;
 
   --foreground-rgb: 0, 0, 0;
   --background-start-rgb: 214, 219, 220;
   --background-end-rgb: 255, 255, 255;
 
-  --primary-glow: conic-gradient(
-    from 180deg at 50% 50%,
-    #16abff33 0deg,
-    #0885ff33 55deg,
-    #54d6ff33 120deg,
-    #0071ff33 160deg,
-    transparent 360deg
-  );
-  --secondary-glow: radial-gradient(
-    rgba(255, 255, 255, 1),
-    rgba(255, 255, 255, 0)
-  );
-
-  --tile-start-rgb: 239, 245, 249;
-  --tile-end-rgb: 228, 232, 233;
-  --tile-border: conic-gradient(
-    #00000080,
-    #00000040,
-    #00000030,
-    #00000020,
-    #00000010,
-    #00000010,
-    #00000080
-  );
-
-  --callout-rgb: 238, 240, 241;
-  --callout-border-rgb: 172, 175, 176;
-  --card-rgb: 180, 185, 188;
-  --card-border-rgb: 131, 134, 135;
-
-  @media (prefers-color-scheme: dark) {
-    --foreground-rgb: 255, 255, 255;
-    --background-start-rgb: 0, 0, 0;
-    --background-end-rgb: 0, 0, 0;
-
-    --primary-glow: radial-gradient(rgba(1, 65, 255, 0.4), rgba(1, 65, 255, 0));
-    --secondary-glow: linear-gradient(
-      to bottom right,
-      rgba(1, 65, 255, 0),
-      rgba(1, 65, 255, 0),
-      rgba(1, 65, 255, 0.3)
-    );
-
-    --tile-start-rgb: 2, 13, 46;
-    --tile-end-rgb: 2, 5, 19;
-    --tile-border: conic-gradient(
-      #ffffff80,
-      #ffffff40,
-      #ffffff30,
-      #ffffff20,
-      #ffffff10,
-      #ffffff10,
-      #ffffff80
-    );
-
-    --callout-rgb: 20, 20, 20;
-    --callout-border-rgb: 108, 108, 108;
-    --card-rgb: 100, 100, 100;
-    --card-border-rgb: 200, 200, 200;
-  }
+  // button
+  --btn-padding: 11px 24px;
+  --btn-xs-padding: 1px 8px;
+  --btn-sm-padding: 3px 16px;
+  --btn-lg-padding: 19px 24px;
+  --btn-radius-base: 9999px;
 `;
 
-export const htmlStyle = css``;
+export const htmlStyle = css`
+  position: relative;
+
+  width: 100%;
+
+  margin: 0;
+  padding: 0;
+
+  /* Do not add overflow: hidden on html style which causing issue with sticky header */
+  scroll-behavior: smooth;
+`;
 
 export const bodyStyle = css`
   color: rgb(var(--foreground-rgb));
-  background: linear-gradient(
-      to bottom,
-      transparent,
-      rgb(var(--background-end-rgb))
-    )
-    rgb(var(--background-start-rgb));
+  font-family: var(--font-sans-serif);
+  letter-spacing: var(--letter-spacing);
+
+  background-color: white;
+
+  /* &.Modal--open {
+    overflow: hidden;
+  } */
+`;
+
+export const h1Style = css`
+  font-size: 2rem;
+`;
+
+export const unstyledListStyle = css`
+  margin: 0;
+  padding: 0;
+
+  list-style: none;
 `;
 
 const GlobalStyle = createGlobalStyle`
@@ -100,7 +85,7 @@ const GlobalStyle = createGlobalStyle`
   html,
   body {
     max-width: 100vw;
-    overflow-x: hidden;
+    /* overflow-x: hidden; */
   }
 
   body {
@@ -112,11 +97,11 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
 
-  @media (prefers-color-scheme: dark) {
+  /* @media (prefers-color-scheme: dark) {
     html {
       color-scheme: dark;
     }
-  }
+  } */
 `;
 
 export default GlobalStyle;
