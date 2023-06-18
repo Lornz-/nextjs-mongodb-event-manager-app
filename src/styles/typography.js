37b392ef23b90e369c5812358d6a2a8bb239889b
lornz- <lrt.hrl@gmail.com>
2023-06-18 18:55:39 -0400

    Add typography styles

---
+++ src/styles/typography.js
@@ -0,0 +1,28 @@
+// vendors
+import { css } from 'styled-components';
+
+export default css`
+  --font-sans-serif: 'Inter', sans-serif;
+  --font-mono: ui-monospace, Menlo, Monaco, 'Cascadia Mono', 'Segoe UI Mono',
+    'Roboto Mono', 'Oxygen Mono', 'Ubuntu Monospace', 'Source Code Pro',
+    'Fira Mono', 'Droid Sans Mono', 'Courier New', monospace;
+
+  --font-size-12: 0.75rem;
+  --font-size-14: 0.875rem;
+  --font-size-16: 1rem;
+  --font-size-18: 1.125rem;
+  --font-size-20: 1.25rem;
+  --font-size-22: 1.375rem;
+  --font-size-24: 1.5rem;
+  --font-size-26: 1.625rem;
+  --font-size-28: 1.75rem;
+  --font-size-30: 1.875rem;
+  --font-size-32: 2rem;
+
+  --letter-spacing-xxs: 0.005em;
+  --letter-spacing-xs: 0.01em;
+  --letter-spacing-sm: 0.0125em;
+  --letter-spacing-md: 0.015em;
+  --letter-spacing-lg: 0.02em;
+  --letter-spacing-xl: 0.025em;
+`;
